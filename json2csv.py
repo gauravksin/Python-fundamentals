1523e45b84c5d95949266377933211b6a129d0e3
gauravksin <gauravks.ger@gmail.com>
2026-04-20 13:14:52 +0200

    Adding json2csv

---
+++ json2csv.py
@@ -0,0 +1,45 @@
+# Program to convert JSON to CSV
+
+import json
+import csv
+import os
+
+INPUT_FILE= 'api_data.json'
+OUTPUT_FILE = 'converted_data.csv'
+
+def load_json(filename):
+    if not os.path.exists(filename):
+        print(f'Error loading file:')
+        return None
+    
+    with open(filename, 'r', encoding ='utf-8') as f:
+        try:
+            return json.load(f)
+        except:
+            print('Invalid JSON format')
+
+
+def convert_to_csv(data, output_file):
+    if not data:
+        print('No data to convert')
+        return
+    
+    fieldname = list(data[0].keys())
+    
+    with open(output_file, 'w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldname)
+        writer.writeheader()
+        for record in data:
+            writer.writerow(record)
+    
+    print(f'Converted {len(data)} records in the CSV file: {output_file}')
+
+def main():
+    print('Converting JSON to CSV...')
+    data = load_json(INPUT_FILE)
+    convert_to_csv(data, OUTPUT_FILE)
+
+
+if __name__ == '__main__':
+    main()
+
